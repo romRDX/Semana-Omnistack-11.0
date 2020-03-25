419d035811603012d30b4374d64d6ebc7d1414a9
Rômullo S <romullo_sas@outlook.com>
2020-03-24 22:45:59 -0300

    Semana Omnistack 11.0: Backend finalizado

---
+++ backend/src/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+
+const app = express();
+
+app.use(cors())
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
+
+/**
+ * Rota / Recurso
+ */
+
+ /**
+  * Métodos HTTP:
+  * 
+  * GET: buscar uma informação do back-end
+  * POST: criar uma informação no back-end
+  * PUT: alterar uma informação no back-end
+  * DELETE: deletar uma informação no back-end
+  */
+
+  /**
+   * Tipos de parametros
+   * 
+   * Query Params: parametros nomeados enviados na rota após ? (filtros, paginação)
+   * Route Params: parametros utilizados para identificar recursos
+   * Request Body: corpo da requisição, utilizado para criar ou alterar recursos
+   */
+
+   /**
+    * SQL: MySQL, SQLite, PostgreSQL, Oracle, Microsoft SQL server
+    * NoSQL: MongoDB, CouchDB, etc
+    */
+
+    /**
+     * Driver: SELECT * FROM users
+     * Query Builder: table('users').select('*').where()
+     */
